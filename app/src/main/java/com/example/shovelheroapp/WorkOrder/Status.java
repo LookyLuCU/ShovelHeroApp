cca17d0fc505a876b76ce8dd9935647973e5a904
LookyLuCU <sheila.donnelly@outlook.com>
2023-11-05 20:02:31 -0800

    Adding some class object packages for housekeeping

---
+++ app/src/main/java/com/example/shovelheroapp/WorkOrder/Status.java
@@ -0,0 +1,5 @@
+package com.example.shovelheroapp.WorkOrder;
+
+public enum Status {
+    Open, Pending, Accepted, Enroute, InProgress, Issue, Completed, CancelledByCustomer, CancelledByShoveller, Closed, OpenCustom;
+}
--- app/src/main/java/com/example/shovelheroapp/WorkOrder/WorkOrder.java
+++ app/src/main/java/com/example/shovelheroapp/WorkOrder/WorkOrder.java
@@ -1,12 +1,29 @@
 package com.example.shovelheroapp.WorkOrder;
 
+import android.media.Image;
+
+import java.util.Date;
+
 public class WorkOrder {
 
 
     private long id;
-    private String customerId; //to be foreign key
+
     private String orderType;
     private String status;
+    private double price;
+    private Date requestDate;
+    private Date requestedDateTime;
+    private Image arrivalImage;
+    private Image completedImage;
+    private Image issueImage;
+
+
+    //private Customer customerId; //to be foreign key
+    //private Property propertyId; //to be foreign key
+    //private Invoice invoiceId; --> is this how we do it? with foreign key?
+    //private Transaction paymentId; --> is this how we do it? Transaction? with foreign key?
+
 
     public WorkOrder(){
 
