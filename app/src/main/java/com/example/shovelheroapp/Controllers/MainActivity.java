0099646f334d4278115aeaf4912a6608d9ed3ea9
LookyLuCU <sheila.donnelly@outlook.com>
2023-11-15 18:48:49 -0800

    added test phrase

--- app/src/main/java/com/example/shovelheroapp/Controllers/MainActivity.java
+++ app/src/main/java/com/example/shovelheroapp/Controllers/MainActivity.java
@@ -18,6 +18,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+//THIS IS A TEST
+
 import android.widget.Spinner;
 
 
