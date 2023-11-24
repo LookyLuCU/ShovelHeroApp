3d2e0c7a840009cb2556c5b928dda7135844f673
Fatimahbintiyasin <113253050+Fatimahbintiyasin@users.noreply.github.com>
2023-11-24 15:49:48 -0500

    Ui design for View Approved WO (Youth and Guardian)

---
+++ app/src/main/java/com/example/shovelheroapp/Controllers/GuardianViewApprovedWOActivity.java
@@ -0,0 +1,16 @@
+package com.example.shovelheroapp.Controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shovelheroapp.R;
+
+public class GuardianViewApprovedWOActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guardian_view_approved_woactivity);
+    }
+}
---
+++ app/src/main/java/com/example/shovelheroapp/Controllers/YouthViewApprovedWOActivity.java
@@ -0,0 +1,16 @@
+package com.example.shovelheroapp.Controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shovelheroapp.R;
+
+public class YouthViewApprovedWOActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youth_view_approved_woactivity);
+    }
+}
