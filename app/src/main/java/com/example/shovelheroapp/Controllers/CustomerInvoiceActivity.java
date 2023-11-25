51c87d09fd60136179825c7400ebefa823ce64d0
Fatimahbintiyasin <113253050+Fatimahbintiyasin@users.noreply.github.com>
2023-11-24 19:11:39 -0500

    UI for Invoice

---
+++ app/src/main/java/com/example/shovelheroapp/Controllers/CustomerInvoiceActivity.java
@@ -0,0 +1,16 @@
+package com.example.shovelheroapp.Controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shovelheroapp.R;
+
+public class CustomerInvoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_invoice);
+    }
+}
---
+++ app/src/main/java/com/example/shovelheroapp/Controllers/YouthAndGuardianInvoiceActivity.java
@@ -0,0 +1,16 @@
+package com.example.shovelheroapp.Controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shovelheroapp.R;
+
+public class YouthAndGuardianInvoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youth_and_guardian_invoice);
+    }
+}
